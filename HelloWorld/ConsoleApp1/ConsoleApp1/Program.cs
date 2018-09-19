cf30419b4dae85a44e106872eba1b255fec588fa
dcjdanger <81524035@qq.com>
2018-09-19 12:03:47 +0800

    clean

---
+++ HelloWorld/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1+2={0}", 1 + 2);
+            Console.ReadKey();//暂停一下，等待按键再返回。
+        }
+    }
+}
